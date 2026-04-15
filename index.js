c06d098ecc7efac705d60a6c5e8767d0242e184c
promisehanyani2-dotcom <promisehanyani2@gmail.com>
2026-04-15 12:43:20 +0200

    Added the searching function using API

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
+function changeWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#place");
+
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchPlace(city) {
+  let apiKey = "e5f2b33t5a4924f02bo250f1a930212e";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  axios.get(apiUrl).then(changeWeather);
+}
+
 function changePlace(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#searching-input");
-  let cityElement = document.querySelector("#place");
-  cityElement.innerHTML = searchInput.value;
+
+  searchPlace(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#searching-form");
