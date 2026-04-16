3e0d8c5fa861df89ecae614011c8d2de7cbb4575
promisehanyani2-dotcom <promisehanyani2@gmail.com>
2026-04-16 16:03:25 +0200

    Update index.js by adding the forecast API

--- index.js
+++ index.js
@@ -16,6 +16,8 @@ function changeWeather(response) {
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
   windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
   temperatureElement.innerHTML = Math.round(temperature);
+
+  getForecast(response.data.city);
 }
 
 function changeDate(date) {
@@ -39,7 +41,7 @@ function changeDate(date) {
 
 function searchPlace(city) {
   let apiKey = "e5f2b33t5a4924f02bo250f1a930212e";
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
   axios.get(apiUrl).then(changeWeather);
 }
 
@@ -50,7 +52,13 @@ function changePlace(event) {
   searchPlace(searchInput.value);
 }
 
-function showForecast() {
+function getForecast(city) {
+  let apiKey = "e5f2b33t5a4924f02bo250f1a930212e";
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showForecast);
+}
+function showForecast(response) {
+  console.log(response.data);
   let days = ["Mon", "Tue", "Wed", "Thu", "Fri"];
   let forecastHtml = "";
 
@@ -72,5 +80,4 @@ function showForecast() {
 }
 let searchFormElement = document.querySelector("#searching-form");
 searchFormElement.addEventListener("submit", changePlace);
-
-showForecast();
+searchPlace("Zimbabwe");
